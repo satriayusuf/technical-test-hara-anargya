f477cc3118d8532722366086a332849ca6b6c96f
satriayusuf <belajarsatria21@gmail.com>
2023-09-28 23:48:14 +0700

    setup main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
-import "./style.css";
 import "../src/assets/css/index.css";
 import App from "./App.vue";
 import { Icon } from "@iconify/vue";
+import router from './router'
 
-createApp(App).component("Icon", Icon).mount("#app");
+const app = createApp(App)
+app.use(router)
+app.component("Icon", Icon)
+app.mount("#app")
